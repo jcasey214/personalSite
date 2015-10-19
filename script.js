fd606e5cfff2376c374a833a8d32f08a251d65b1
Jonathan Casey <jon_casey@me.com>
2015-10-18 20:08:34 -0600

    Update text on pages

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$('button').click(function(){
-		$('img').toggleClass('hidden');
-		$('button').toggleClass('hidden');
+		$('#left img').toggleClass('hidden');
+		$('#left button').toggleClass('hidden');
 	});
 	$('button').hover(function(){
 		$(this).animate({fontSize:'1.35em'},200);
 	},function(){
-		$(this).animate({fontSize:'1.25em'},200)
+		$(this).animate({fontSize:'1.25em'},200);
 	});
 });
 
